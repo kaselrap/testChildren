70cf68228669095af8d26fe7de9774a18ec1909f
kaselrap <kaselrap@gmail.com>
2018-11-05 08:01:52 +0000

    Changed shown excel info

--- admin/include/downloadXlsFile.php
+++ admin/include/downloadXlsFile.php
@@ -1,21 +1,26 @@
 <?php 
- //download.php page code
- //THIS PROGRAM WILL FETCH THE RESULT OF SQL QUERY AND WILL DOWNLOAD IT. (IF YOU HAVE ANY QUERY CONTACT:rahulpatel541@gmail.com)
-//include the database file connection
 include '../../functions.php';
 
 //will work if the link is set in the indx.php page
 if(isset($_GET['name']))
 {
+    error_reporting(0);
     $name=$_GET['name']; //to rename the file
-    header('Content-Disposition: attachment; filename='.$name.'.xls'); 
-    header('Cache-Control: no-cache, no-store, must-revalidate, post-check=0, pre-check=0');
-    header('Pragma: no-cache');
-    header('Content-Type: application/x-msexcel; charset=windows-1251; format=attachment;');
+    // header("Pragma: public");
+    // header('Content-Disposition: attachment; filename='.$name.'.xls'); 
+    // header('Cache-Control: no-cache, no-store, must-revalidate, post-check=0, pre-check=0');
+    // header('Content-Type: application/x-msexcel; charset=utf-8; format=attachment;');
     $msg="";
     $var="";
-    //write your query      
-    $msg = 
+    //write your query  
+    echo "<style>table {
+        border-collapse: collapse;
+    }
+    
+    table, th, td {
+        border: 1px solid black;
+    } </style>";    
+    echo 
     "
     <table>
         <thead>
@@ -35,19 +40,25 @@ if(isset($_GET['name']))
     $problems =R::findAll('problems');
     foreach ($problems as $problem) {
         $object = getQuestionById((int)$problem->id_question);
-        $msg .= "<tr>";
-        $msg .= "<td>" . $object->type . "</td>";
-        $object = json_decode($object->question);
+        echo "<tr>";
+        echo "<td>" . $object->type . "</td>";
+        $object = json_decode($object->question, 1);
         foreach ($object as $key => $value) {    
-            $msg .= "<td>" . $value . "</td>";
+            echo "<td>" . $value . "</td>";
         }
-        $msg .= "<td>" . $problem->gender . "</td>";
-        $msg .= "<td>" . $problem->problem . "</td>";
-        $msg .= "<td>" . $problem->id_question . "</td>";
-        $msg .= "</tr>";
+        echo "<td>" . $problem->gender . "</td>";
+        $problema = json_decode( $problem->problem);
+        foreach ($problema as $item) {
+            echo "<tr>";
+            echo "<td width='180px'>Problem name: ". $item->problemName ."</td>";
+            foreach ($item->problemList as $answer) {
+                echo "<td width='180px'>Decision of problem: ". $answer->answer ."</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</tr>";
     }
-
-    $msg.="</tbody></table>";
-    echo $msg;  //will print the content in the exel page
+    
+    echo "</tbody></table>";
 }
 ?>
